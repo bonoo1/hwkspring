6514bbf497e6c304533f786b533457e79427c8dc
bonoo1 <bonoo74@gmail.com>
2022-02-12 11:17:53 +0900

    Add Evice to Leopards

---
+++ sparta_java/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String[] args) {
+        int number = 5;
+        System.out.println(number);
+
+        String sparta = "Hello Sparta";
+        System.out.println(sparta);
+
+        final int finalNumber = 22;
+        System.out.println(finalNumber);
+
+
+    }
+
+}
